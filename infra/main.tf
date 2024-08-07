6ceb95fb5d4a7a5a447ceb82dee521bd4ceeb0db
OdirleiK <odirlei67777@gmail.com>
2024-08-07 17:17:14 -0300

    :sparkles: Altered ami aws to ami ubuntu and add ingress to Jenkins

--- infra/main.tf
+++ infra/main.tf
@@ -6,11 +6,19 @@ resource "aws_security_group" "securitygroup" {
   name = "securitygroup"
   description = "Permitir acesso HTTP e acesso a Internet"
 
-  #tregra de entrada
+  #regra de entrada
   ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Regra de entrada para Jenkins (porta 8080)
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -37,9 +45,9 @@ resource "aws_key_pair" "keypair" {
 }
 
 resource "aws_instance" "servidor" {
-  ami            = "ami-0ba9883b710b05ac6"
-  instance_type  = "t2.nano"
-  user_data = file("user_data.sh")
+  ami            = "ami-04a81a99f5ec58529"
+  instance_type  = "t2.micro"
+  #user_data = file("user_data.sh")
   key_name = aws_key_pair.keypair.key_name
   vpc_security_group_ids = [ aws_security_group.securitygroup.id ]
 }
